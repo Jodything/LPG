378c6944d37ec97734af0fac53e315157bb61121
Jodything <Jodything@gmail.com>
2014-12-19 15:51:24 -0500

    fixed autogrow text area form

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,11 +1,12 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def all
     user = User.from_omniauth(request.env["omniauth.auth"])
-    if user.persisted? && ["aatcarriers.com", "ambition.com", "americanexchange.com", "chattanoogawhiskey.com",
-                           "fanjam.com", "fancyrhino.com", "getbellhops.com","lamppostgroup.com",
-                           "morningdoveservices.com", "noog.com", "pando.com", "pricewaiter.com", "readycart.com",
-                           "reliancepartners.com", "steamlogistics.com","supplyhog.com", "wampstrategy.com",
-                           "waypaver.co"].include?(user.company.downcase)
+    if user.persisted?
+      # && ["aatcarriers.com", "ambition.com", "americanexchange.com", "chattanoogawhiskey.com",
+      #                      "fanjam.com", "fancyrhino.com", "getbellhops.com","lamppostgroup.com",
+      #                      "morningdoveservices.com", "noog.com", "pando.com", "pricewaiter.com", "readycart.com",
+      #                      "reliancepartners.com", "steamlogistics.com","supplyhog.com", "wampstrategy.com",
+      #                      "waypaver.co"].include?(user.company.downcase)
 
       flash.notice = "You are now logged in"
       sign_in_and_redirect user
