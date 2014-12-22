926ff5950fb8f7e696e9d2a15a742e1398b96321
Jodything <Jodything@gmail.com>
2014-12-22 12:22:55 -0500

    messed with navigation and cms nav

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   comfy_route :cms_admin, :path => '/admin'
 
   # Make sure this routeset is defined last
-  comfy_route :cms, :path => '/company_info/index', :sitemap => true
+  comfy_route :cms, :path => '/', :sitemap => true
 
 
 end
