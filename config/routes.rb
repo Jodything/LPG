5fc910688ebe271610b6894dc3910dc8144783f9
Jodything <Jodything@gmail.com>
2015-01-06 10:47:25 -0500

    changed root to blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
-  match '/contacts',     to: 'contacts#new',             via: 'get'
+  match '/contacts', to: 'contacts#new', via: 'get'
   resources "contacts", only: [:new, :create]
 
   devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
-  resources :articles
+  # resources :articles
   get 'welcome/index'
-  root 'articles#index'
-  # get 'company_info/index'
+  root :to => 'comfy/blog/posts#index'
+
 	# This line mounts Forem's routes at /forums by default.
 	# This means, any requests to the /forums URL of your application will go to Forem::ForumsController#index.
 	# If you would like to change where this extension is mounted, simply change the :at option to something different.
